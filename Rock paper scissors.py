2d47be46c4aaf7591e2dc7edb877eda686cc7e8d
Ankush <akrk053@gmail.com>
2026-03-30 13:46:13 +0530

    Update Rock paper scissors.py

--- Rock paper scissors.py
+++ Rock paper scissors.py
@@ -6,6 +6,7 @@ print('Winning rules of the game ROCK PAPER SCISSORS are:\n'
       + "Rock vs Scissors -> Rock wins \n"
       + "Paper vs Scissors -> Scissors wins \n")
 
+
 while True:
 
     print("Enter your choice \n 1 - Rock \n 2 - Paper \n 3 - Scissors \n")
