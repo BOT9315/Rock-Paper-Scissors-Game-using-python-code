d9bfad90acc20089c41f629c48cb16f906da9904
Ankush <akrk053@gmail.com>
2026-04-04 11:36:11 +0530

    Reformat Rock Paper Scissors game code

--- Rock paper scissors.py
+++ Rock paper scissors.py
@@ -5,7 +5,6 @@ print('Winning rules of the game ROCK PAPER SCISSORS are:\n'
       + "Rock vs Scissors -> Rock wins \n"
       + "Paper vs Scissors -> Scissors wins \n")
 
-
 while True:
     print("Enter your choice \n 1 - Rock \n 2 - Paper \n 3 - Scissors \n")
 
